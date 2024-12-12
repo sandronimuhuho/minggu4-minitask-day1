eb7e0610ea8c4bbf391753637ef17f5fa443cd92
sandro nimunuho <sandronimunuo@gmail.com>
2024-12-12 15:12:02 +0800

    minitask minggu4 day1 (dom.js)

---
+++ dom.js
@@ -0,0 +1,29 @@
+// nomor 1
+const title = document.title;
+console.log(title)
+
+
+// nomor 2
+
+// const width_window = window.innerWidth;
+// const height_window = window.innerHeight;
+// console.log(width_window);
+// console.log(height_window);
+
+//nomor 3
+
+// const email = document.getElementById("email-id");
+// console.log(email);
+
+// nomor 4 
+
+// const ewallet = document.getElementsByClassName("e-walet-img");
+// console.log(ewallet);
+
+
+// nomor 5
+
+// const btn = document.getElementsByClassName("btn");
+// console.log(btn);
+
+
